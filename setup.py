c6386f7a4e67a0558ec68fce26ed62565253f94b
Ivan Usov <ivan.usov@psi.ch>
2018-03-15 12:08:14 +0100

    Updating for version 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="streamvis",
-      version="0.1.5",
+      version="0.1.6",
       author="Ivan Usov",
       author_email="ivan.usov@psi.ch",
       description="Stream visualization of JF data.",
