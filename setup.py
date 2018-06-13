f72fd5fd3094c3156ef3ae5b9d7b1e37a7a7c6e0
Ivan Usov <ivan.usov@psi.ch>
2018-06-13 14:53:27 +0200

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from setuptools import setup
 
-setup(name="streamvis",
-      version="0.2.3",
-      author="Ivan Usov",
-      author_email="ivan.usov@psi.ch",
-      description="Stream visualization of JF data.",
-      packages=['streamvis'],
-      license='GNU GPLv3',
-      requires=['bokeh',
-                'colorcet',
-                'numpy',
-                'pyzmq',
-                'matplotlib',
-                'hdf5plugin',
-                'h5py',
-                'pillow',
-                'tornado',
-               ],
-     )
+setup(
+    name="streamvis",
+    version="0.2.3",
+    author="Ivan Usov",
+    author_email="ivan.usov@psi.ch",
+    description="Live stream visualization server for detectors at PSI",
+    packages=["streamvis"],
+    license="GNU GPLv3",
+)
