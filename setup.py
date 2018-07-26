d1bca4c00c12ea78ade650406f473554b494d15c
Ivan Usov <ivan.usov@psi.ch>
2018-07-26 13:17:44 +0200

    Updating for version 0.3.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="streamvis",
-    version="0.3.4",
+    version="0.3.5",
     author="Ivan Usov",
     author_email="ivan.usov@psi.ch",
     description="Live stream visualization server for detectors at PSI",
