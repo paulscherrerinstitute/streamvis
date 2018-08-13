f3ae9d968a1f530ec5e4db6569f4ca9930766847
Ivan Usov <ivan.usov@psi.ch>
2018-08-13 15:15:17 +0200

    replace os.system() with subrocess.run()
    subrocess.run() is the recommended approach to invoking subprocesses
    in python/3.6

--- streamvis/streamvis.py
+++ streamvis/streamvis.py
@@ -2,8 +2,8 @@ def main():
     """
     Execute the "streamvis" command line program.
 
-    This is a wrapper around 'bokeh serve' command which provides a user interface to launch applications
-    bundled with streamvis package.
+    This is a wrapper around 'bokeh serve' command which provides a user interface to launch
+    applications bundled with the streamvis package.
 
     For more information, see:
     https://bokeh.pydata.org/en/latest/docs/reference/command/subcommands/serve.html
@@ -11,6 +11,7 @@ def main():
 
     import os
     import sys
+    import subprocess
 
     apps_path = os.path.join(os.environ['CONDA_PREFIX'], 'streamvis-apps')
 
@@ -19,9 +20,9 @@ def main():
     # which is supposed to be fixed in python/3.7, keep parsing unflexible, but very simple
     _, app_name, *app_args = sys.argv
 
-    bokeh_command = ' '.join(['bokeh serve', os.path.join(apps_path, app_name), ' '.join(app_args)])
-    print(bokeh_command)
-    os.system(bokeh_command)
+    command = ['bokeh', 'serve', os.path.join(apps_path, app_name), *app_args]
+    print(' '.join(command))
+    subprocess.run(command)
 
 if __name__ == "__main__":
     main()
