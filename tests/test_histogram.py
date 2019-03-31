9767b4237519c947fb4046bbd4f236e9b05d0740
Ivan Usov <ivan.usov@psi.ch>
2019-03-31 20:30:53 +0200

    all: fix histogram tests
    broken after ec5eefec1e96f32a23d15f152bc16d21c5e4a707

--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -28,19 +28,21 @@ def test_plot_sizes(plot_height, plot_width):
 @pytest.mark.parametrize("value", [0, -1, 1, 350, 800, -1000])
 def test_update_single_value(sv_hist_single_plot, value):
     data = [np.array(value)]
+    default_nbins = sv_hist_single_plot._nbins
     sv_hist_single_plot.update(data)
 
-    assert len(sv_hist_single_plot._plot_sources[0].data['left']) == 1
-    assert len(sv_hist_single_plot._plot_sources[0].data['right']) == 1
-    assert len(sv_hist_single_plot._plot_sources[0].data['top']) == 1
+    assert len(sv_hist_single_plot._plot_sources[0].data['left']) == default_nbins
+    assert len(sv_hist_single_plot._plot_sources[0].data['right']) == default_nbins
+    assert len(sv_hist_single_plot._plot_sources[0].data['top']) == default_nbins
 
 def test_update_array(sv_hist_single_plot):
     data = [np.random.randint(-100, 100, 1000)]
+    default_nbins = sv_hist_single_plot._nbins
     sv_hist_single_plot.update(data)
 
-    assert len(sv_hist_single_plot._plot_sources[0].data['left']) == 10
-    assert len(sv_hist_single_plot._plot_sources[0].data['right']) == 10
-    assert len(sv_hist_single_plot._plot_sources[0].data['top']) == 10
+    assert len(sv_hist_single_plot._plot_sources[0].data['left']) == default_nbins
+    assert len(sv_hist_single_plot._plot_sources[0].data['right']) == default_nbins
+    assert len(sv_hist_single_plot._plot_sources[0].data['top']) == default_nbins
 
 @pytest.mark.parametrize("nbins", [1, 300, 1000])
 def test_update_fixed(nbins):
