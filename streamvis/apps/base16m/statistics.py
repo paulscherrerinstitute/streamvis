48af185eedff20950efdffc5d44bbfd7d061667a
Ivan Usov <ivan.usov@psi.ch>
2019-05-19 21:31:08 +0200

    base16m: make statistics layout stretchable

--- streamvis/apps/base16m/statistics.py
+++ streamvis/apps/base16m/statistics.py
@@ -29,23 +29,11 @@ table_columns = [
 ]
 
 table_source = ColumnDataSource(receiver.stats_table_dict)
-table = DataTable(
-    source=table_source,
-    columns=table_columns,
-    width=1380,
-    height=750,
-    index_position=None,
-    selectable=False,
-)
+table = DataTable(source=table_source, columns=table_columns, height=50, index_position=None)
 
 sum_table_source = ColumnDataSource(receiver.sum_stats_table_dict)
 sum_table = DataTable(
-    source=sum_table_source,
-    columns=table_columns,
-    width=1380,
-    height=50,
-    index_position=None,
-    selectable=False,
+    source=sum_table_source, columns=table_columns, height=50, index_position=None,
 )
 
 
@@ -84,7 +72,12 @@ def reset_stats_button_callback():
 reset_stats_button = Button(label="Reset Statistics", button_type='default')
 reset_stats_button.on_click(reset_stats_button_callback)
 
-layout = column(table, sum_table, reset_stats_button)
+layout = column(
+    column(table, sizing_mode="stretch_both"),
+    sum_table,
+    reset_stats_button,
+    sizing_mode="stretch_width",
+)
 
 doc.add_root(layout)
 doc.add_periodic_callback(update_statistics, 1000)
