046e504d0af5a20a8b0b90832e4a4f5341625dc8
Ivan Usov <ivan.usov@psi.ch>
2020-07-14 16:01:45 +0200

    Turn make_release into a script

--- make_release.py
+++ make_release.py
@@ -1,34 +1,42 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
 import re
 
-filepath = "streamvis/__init__.py"
 
-parser = argparse.ArgumentParser()
-parser.add_argument("level", type=str, choices=["patch", "minor", "major"])
-parser.add_argument("tag_msg", type=str, help="tag message")
-args = parser.parse_args()
+def main():
+    filepath = "streamvis/__init__.py"
 
-with open(filepath) as f:
-    file_content = f.read()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("level", type=str, choices=["patch", "minor", "major"])
+    parser.add_argument("tag_msg", type=str, help="tag message")
+    args = parser.parse_args()
 
-version = re.search(r'__version__ = "(.*?)"', file_content).group(1)
-major, minor, patch = map(int, version.split(sep="."))
+    with open(filepath) as f:
+        file_content = f.read()
 
-if args.level == "patch":
-    patch += 1
-elif args.level == "minor":
-    minor += 1
-    patch = 0
-elif args.level == "major":
-    major += 1
-    minor = 0
-    patch = 0
+    version = re.search(r'__version__ = "(.*?)"', file_content).group(1)
+    major, minor, patch = map(int, version.split(sep="."))
 
-new_version = f"{major}.{minor}.{patch}"
+    if args.level == "patch":
+        patch += 1
+    elif args.level == "minor":
+        minor += 1
+        patch = 0
+    elif args.level == "major":
+        major += 1
+        minor = 0
+        patch = 0
 
-with open(filepath, "w") as f:
-    f.write(re.sub(r'__version__ = "(.*?)"', f'__version__ = "{new_version}"', file_content))
+    new_version = f"{major}.{minor}.{patch}"
 
-os.system(f"git commit {filepath} -m 'Updating for version {new_version}'")
-os.system(f"git tag -a {new_version} -m '{args.tag_msg}'")
+    with open(filepath, "w") as f:
+        f.write(re.sub(r'__version__ = "(.*?)"', f'__version__ = "{new_version}"', file_content))
+
+    os.system(f"git commit {filepath} -m 'Updating for version {new_version}'")
+    os.system(f"git tag -a {new_version} -m '{args.tag_msg}'")
+
+
+if __name__ == "__main__":
+    main()
