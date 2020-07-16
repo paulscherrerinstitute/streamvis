536b5a0b748ca9bb6dd44da75ae592f24c81687a
Ivan Usov <ivan.usov@psi.ch>
2020-07-16 20:56:18 +0200

    Swap colors for fast and slow hitrate lines

--- streamvis/common_apps/hitrate.py
+++ streamvis/common_apps/hitrate.py
@@ -53,24 +53,24 @@ plot.add_layout(LinearAxis(axis_label="Hitrate"), place="left")
 plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
 plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
 
-# ---- red step glyph
-step_red_source = ColumnDataSource(dict(x=[], y=[]))
-step_red = plot.add_glyph(
-    step_red_source, Step(x="x", y="y", mode="after", line_color="red", line_width=2)
+# ---- blue step glyph
+step_fast_source = ColumnDataSource(dict(x=[], y=[]))
+step_fast = plot.add_glyph(
+    step_fast_source, Step(x="x", y="y", mode="after", line_color="steelblue", line_width=2)
 )
 
-# ---- blue step glyph
-step_blue_source = ColumnDataSource(dict(x=[], y=[]))
-step_blue = plot.add_glyph(
-    step_blue_source, Step(x="x", y="y", mode="after", line_color="steelblue", line_width=2)
+# ---- red step glyph
+step_slow_source = ColumnDataSource(dict(x=[], y=[]))
+step_slow = plot.add_glyph(
+    step_slow_source, Step(x="x", y="y", mode="after", line_color="red", line_width=2)
 )
 
 # ---- legend
 plot.add_layout(
     Legend(
         items=[
-            (f"{stats.hitrate_fast.step_size} pulse ids avg", [step_red]),
-            (f"{stats.hitrate_slow.step_size} pulse ids avg", [step_blue]),
+            (f"{stats.hitrate_fast.step_size} pulse ids avg", [step_fast]),
+            (f"{stats.hitrate_slow.step_size} pulse ids avg", [step_slow]),
         ],
         location="top_left",
     )
@@ -80,13 +80,13 @@ plot.legend.click_policy = "hide"
 
 # Reset button
 def reset_button_callback():
-    data = step_red_source.data
+    data = step_fast_source.data
     if data["x"]:
-        step_red_source.data.update(dict(x=[data["x"][-1]], y=[data["y"][-1]]))
+        step_fast_source.data.update(dict(x=[data["x"][-1]], y=[data["y"][-1]]))
 
-    data = step_blue_source.data
+    data = step_slow_source.data
     if data["x"]:
-        step_blue_source.data.update(dict(x=[data["x"][-1]], y=[data["y"][-1]]))
+        step_slow_source.data.update(dict(x=[data["x"][-1]], y=[data["y"][-1]]))
 
 
 reset_button = Button(label="Reset", button_type="default", disabled=True)
@@ -100,10 +100,10 @@ def update():
         return
 
     x_fast, y_fast = stats.hitrate_fast.values
-    step_red_source.data.update(dict(x=x_fast, y=y_fast))
+    step_fast_source.data.update(dict(x=x_fast, y=y_fast))
 
     x_slow, y_slow = stats.hitrate_slow.values
-    step_blue_source.data.update(dict(x=x_slow, y=y_slow))
+    step_slow_source.data.update(dict(x=x_slow, y=y_slow))
 
 
 doc.add_root(
