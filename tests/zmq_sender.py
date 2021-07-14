c84dc276f7698be62cc8f2987371d38f3985a716
Ivan Usov <ivan.usov@psi.ch>
2021-07-14 14:26:14 +0200

    Update zmq_sender

--- tests/zmq_sender.py
+++ tests/zmq_sender.py
@@ -1,5 +1,4 @@
 import argparse
-import random
 import sys
 from itertools import cycle
 from time import sleep
@@ -15,7 +14,10 @@ args = parser.parse_args()
 
 if args.stream == "base":
     im_size_x = 1024
-    im_size_y = 512
+    im_size_y = 1024
+elif args.stream == "raw-16m":
+    im_size_x = 1024
+    im_size_y = 512*32
 elif args.stream == "alvra":
     im_size_x = 9318
     im_size_y = 514
@@ -48,10 +50,10 @@ def simul_image_gen(sim_im_size_x=im_size_x, sim_im_size_y=im_size_y, dtype=np.u
 
     images = []
     for i in range(4):
-        _im = np.random.uniform(0, 90, size=(sim_im_size_y, sim_im_size_x)).astype(dtype)
+        _im = np.random.uniform(5000, 10090, size=(sim_im_size_y, sim_im_size_x)).astype(dtype)
         _im[cy[i] - sy[i] : cy[i] + sy[i], cx[i] - sx[i] : cx[i] + sx[i]] += np.random.uniform(
-            0, 30, size=(2 * sy[i], 2 * sx[i])
-        ).astype(dtype) + 2 * np.arange(2 * sx[i], dtype=dtype)
+            0, 3000, size=(2 * sy[i], 2 * sx[i])
+        ).astype(dtype) + 20 * np.arange(2 * sx[i], dtype=dtype)
         _im[100:200, 150:250] = 0
         images.append(_im.astype(dtype, copy=False))
 
@@ -60,7 +62,7 @@ def simul_image_gen(sim_im_size_x=im_size_x, sim_im_size_y=im_size_y, dtype=np.u
 
 def send_array(socket, array, frame_num, pulse_id, flags=0, copy=False, track=False):
     """send a numpy array with metadata"""
-    n_spots = int(np.random.uniform(0, 20))
+    n_spots = int(np.random.uniform(0, 40))
     md = dict(
         htype=["array-1.0"],
         type=str(array.dtype),
@@ -75,18 +77,26 @@ def send_array(socket, array, frame_num, pulse_id, flags=0, copy=False, track=Fa
         number_of_spots=n_spots,
         spot_x=list(np.random.rand(n_spots) * 1000),
         spot_y=list(np.random.rand(n_spots) * 1000),
-        pedestal_file="/test_path/pedestal_20181206_0754.JF06T32V01.res.h5",
-        detector_name="JF06T32V01",
+        pedestal_file="/home/usov_i/pedestals/run_000089.JF06T32V02.res.h5",
+        gain_file="/home/usov_i/gains/gains.2020-08.h5",
+        detector_name="JF07T32V01",
         run_name="run_001",
         detector_distance=0.015,
         beam_energy=4570.0,
-        beam_center_x=2215,
-        beam_center_y=2108,
+        beam_center_x=700,
+        beam_center_y=500,
         swissmx_x=frame_num % 10,
         swissmx_y=frame_num // 10,
         laser_on=(frame_num % 4 == 0),
+        radint_q=[0, 10],
+        radint_I=list(np.random.rand(10) * frame_num),
         saturated_pixels=int(np.random.uniform(0, 3)),
     )
+
+    if "raw" not in args.stream:
+        del md["detector_name"]
+        del md["pedestal_file"]
+        del md["gain_file"]
 
     socket.send_json(md, flags | zmq.SNDMORE)
     return socket.send(array, flags, copy=copy, track=track)
@@ -97,12 +107,17 @@ if __name__ == "__main__":
     ctx = zmq.Context()
     skt = ctx.socket(zmq.PUB)  # pylint: disable=E1101
     skt.bind("tcp://127.0.0.1:9001")
-    im_gen = simul_image_gen()
+    if "raw" in args.stream:
+        dtype = np.uint16
+    else:
+        dtype = np.float32
+    im_gen = simul_image_gen(dtype=dtype)
 
     for n in range(N):
         send_array(skt, next(im_gen), n, pulseid)
-        pulseid += 100 + round(random.uniform(-15, 15))
-        sleep(1)
+        # pulseid += 20 + round(random.uniform(-15, 15))
+        pulseid += 20
+        sleep(0.2)
 
     skt.close()
     sys.exit()
