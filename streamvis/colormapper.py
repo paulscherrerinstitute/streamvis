Correct limits calculation

streamvis/colormapper.py
@@ -178,8 +178,8 @@ class ColorMapper:
         """
         if self.auto_switch.active:
             # This is faster than using bottleneck versions of nanmin and nanmax
-            image_min = int(np.nanmin(image))
-            image_max = int(np.nanmax(image))
+            image_min = np.floor(np.nanmin(image))
+            image_max = np.ceil(np.nanmax(image))
 
             if image_min <= 0:  # switch to linear colormap
                 self.scale_radiogroup.active = 0

streamvis/histogram.py
@@ -164,8 +164,8 @@ class Histogram:
                 max_val = 1 if np.isnan(max_val) else max_val
                 upper = max(upper, max_val)
 
-            self.lower_spinner.value = int(np.floor(lower))
-            self.upper_spinner.value = int(np.ceil(upper))
+            self.lower_spinner.value = np.floor(lower)
+            self.upper_spinner.value = np.ceil(upper)
 
         # get histogram counts and update plots
         for i, data in enumerate(input_data):